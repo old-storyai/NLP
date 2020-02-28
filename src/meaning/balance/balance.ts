09818d3100b6e63a656c489769ea98fd06eccb9d
Baptiste Zorzi <baptiste.zorzi@gmail.com>
2020-02-28 18:51:36 +0100

    adding rigging mechanism to the balance

--- src/meaning/balance/balance.ts
+++ src/meaning/balance/balance.ts
@@ -3,7 +3,6 @@ export default class Balance {
 
     /**
      * The data should look like:
-     *
      * {
      *   "category1": {
      *     "word1": weight1,
@@ -15,8 +14,31 @@ export default class Balance {
      */
     data: object;
 
+    /**
+     * Allows to add some weight to a specific category:
+     * {
+     *   "category1": 50,
+     *   ...
+     * }
+     */
+    rigging: object;
+
     constructor(configData: object) {
         this.data = configData;
+        this.rigging = {};
+    }
+
+    rig(rigWeights: object): void {
+        for (const cat of Object.keys(rigWeights)) {
+            if (cat in this.rigging)
+                this.rigging[cat] += rigWeights[cat];
+            else
+                this.rigging[cat] = rigWeights[cat];
+        }
+    }
+
+    unrig(): void {
+        this.rigging = {};
     }
 
     categorize(sentence: string): string {
@@ -35,12 +57,17 @@ export default class Balance {
     getWeightDetails(sentence: string): object {
         const weightPerCategory = {};
         for (const category of Object.keys(this.data)) {
+
+            if (category in this.rigging)
+                weightPerCategory[category] = this.rigging[category];
+
+
             for (const word of Object.keys(this.data[category])) {
                 const weight = this.data[category][word];
                 if (!(category in weightPerCategory))
                     weightPerCategory[category] = 0;
 
-                if (new RegExp('\\b'+word+'\\b', "gi").test(sentence))
+                if (new RegExp('\\b'+word+'\\b', 'gi').test(sentence))
                     weightPerCategory[category] += weight;
             }
         }
